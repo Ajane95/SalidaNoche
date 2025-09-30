7eaecb1f1660533e82f9603cc30923e26e7f85eb
Amanda <amandajaneirovazquez95@liceolapaz.net>
2025-09-30 13:44:14 +0200

    Hecho hasta el 7, falta el 8

--- src/SalidaNoche.java
+++ src/SalidaNoche.java
@@ -4,10 +4,6 @@ public class SalidaNoche {
     public static void main(String[] args) {
         System.out.println("Vamos a tomar el aire. Bienvenido");
         //
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese su nombre: ");
-        String nombreUsuario = sc.nextLine();
-        //
         int nivelAlcohol = 0.0;
         int precioChupito = 3;
         int precioCaña = 1.8;
@@ -17,17 +13,30 @@ public class SalidaNoche {
         int nivelCombinado = 0.4;
         //
         Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese su nombre: ");
+        String nombreCliente = sc.nextLine();
+        //
+        Scanner sc = new Scanner(System.in);
         System.out.println("Introduzca su edad");
-        int edad = sc.nextInt();
-        if (edad < 18){
+        int edadCliente = sc.nextInt();
+        //
+        int dineroCliente = 15;
+        //
+        System.out.println("El cliente" + nombreCliente + "con edad" + edadCliente + "," + "tiene 15 euros");
+        //
+        System.out.println("Comprobación de edad del cliente");
+        if (edadCliente < 18){
             System.out.println("Denegada la entrada");
         }else {
             System.out.println("El cliente es mayor de edad");
         }
         int nivelAlcohol = sc.nextInt();
-        if (nivelAlcohol > 1.2){
+        if (nivelAlcohol < 1.2){
+            System.out.println("El cliente puede entrar");
+        } else {
             System.out.println("El cliente no está en condiciones de entrar");
-            }
+
+        }
         }
 
     }
